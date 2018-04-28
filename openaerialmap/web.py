dd617a60376680e7d6a553e873c5f737ddf3ca72
Seth Fitzsimmons <seth@mojodna.net>
2018-04-27 17:47:06 -0700

    Use user-specific endpoints when mosaicking

--- openaerialmap/web.py
+++ openaerialmap/web.py
@@ -112,15 +112,10 @@ def meta(id, scene_idx, image_id=None, prefix=None):
     return jsonify(meta)
 
 
-@app.route("/o/<path:id>/")
-@app.route("/<prefix>/o/<path:id>/")
+@app.route("/user/<path:id>/")
+@app.route("/<prefix>/user/<path:id>/")
 def remote_meta(id, prefix=None):
-    # prefix is for URL generation only (API Gateway stages); if it matched the
-    # URL, it's part of the id
-    if prefix is not None:
-        id = "/".join([prefix, id])
-
-    catalog = make_remote_catalog(id)
+    catalog = make_remote_catalog("user", id)
 
     meta = {
         "bounds": catalog.bounds,
@@ -223,16 +218,11 @@ def preview(id, scene_idx, image_id=None, prefix=None):
         }
 
 
-@app.route("/o/<path:id>/preview")
-@app.route("/<prefix>/o/<path:id>/preview")
+@app.route("/user/<path:id>/preview")
+@app.route("/<prefix>/user/<path:id>/preview")
 def remote_preview(id, prefix=None):
-    # prefix is for URL generation only (API Gateway stages); if it matched the
-    # URL, it's part of the id
-    if prefix is not None:
-        id = "/".join([prefix, id])
-
     # load the catalog so it will fail if the source doesn't exist
-    make_remote_catalog(id)
+    make_remote_catalog("user", id)
 
     with app.app_context():
         return render_template(
@@ -281,17 +271,12 @@ def render_png(id, scene_idx, z, x, y, image_id=None, scale=1, prefix=None):
     return data, 200, headers
 
 
-@app.route("/o/<path:id>/<int:z>/<int:x>/<int:y>.png")
-@app.route("/o/<path:id>/<int:z>/<int:x>/<int:y>@<int:scale>x.png")
-@app.route("/<prefix>/o/<path:id>/<int:z>/<int:x>/<int:y>.png")
-@app.route("/<prefix>/o/<path:id>/<int:z>/<int:x>/<int:y>@<int:scale>x.png")
+@app.route("/user/<path:id>/<int:z>/<int:x>/<int:y>.png")
+@app.route("/user/<path:id>/<int:z>/<int:x>/<int:y>@<int:scale>x.png")
+@app.route("/<prefix>/user/<path:id>/<int:z>/<int:x>/<int:y>.png")
+@app.route("/<prefix>/user/<path:id>/<int:z>/<int:x>/<int:y>@<int:scale>x.png")
 def render_png_from_remote(id, z, x, y, scale=1, prefix=None):
-    # prefix is for URL generation only (API Gateway stages); if it matched the
-    # URL, it's part of the id
-    if prefix is not None:
-        id = "/".join([prefix, id])
-
-    catalog = make_remote_catalog(id)
+    catalog = make_remote_catalog("user", id)
     tile = Tile(x, y, z)
 
     headers, data = tiling.render_tile(
