109ade55ca32cd8e4fbbb8c5c41ed7220246725a
Seth Fitzsimmons <seth@mojodna.net>
2018-07-06 11:02:10 -0700

    Use sys.exit properly

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from __future__ import division, print_function
 import logging
 import os
 import signal
+import sys
 
 from openaerialmap.web import app
 
@@ -12,8 +13,8 @@ LOG = logging.getLogger(__name__)
 
 
 def handler(signum, frame):
-    LOG.warning('Request timed out; crashing in place of cleanup.')
-    exit(1)
+    LOG.warning("Request timed out; crashing in place of cleanup.")
+    sys.exit(1)
 
 
 # Register the signal function handler
@@ -21,6 +22,7 @@ signal.signal(signal.SIGALRM, handler)
 
 
 class TimeoutMiddleware:
+
     def __init__(self, app, timeout):
         self.timeout = timeout
         self.wrapped_app = app
@@ -36,13 +38,15 @@ class TimeoutMiddleware:
 
 
 class HostMiddleware:
+
     def __init__(self, app):
         self.wrapped_app = app
 
     def __call__(self, environ, start_response):
         # treat X-Forwarded-Host as Host
-        environ["HTTP_HOST"] = environ.get("HTTP_X_FORWARDED_HOST",
-                                           environ.get("HTTP_HOST"))
+        environ["HTTP_HOST"] = environ.get(
+            "HTTP_X_FORWARDED_HOST", environ.get("HTTP_HOST")
+        )
 
         return self.wrapped_app(environ, start_response)
 
